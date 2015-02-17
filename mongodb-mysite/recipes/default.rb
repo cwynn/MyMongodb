d0d384157f68a71e8c3d026073634622b8bc1acc
cwynn <cwynn@gascard.net>
2015-02-16 18:25:51 -0600

    Added fail2ban package to default.rb

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -3,3 +3,9 @@ node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instanc
 
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
+
+package 'fail2ban'
+
+service 'fail2ban' do
+  action [:start, :enable]
+end
