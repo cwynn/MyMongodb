7b689b648fbf2920a21af7ad09abf881a11739c9
cwynn <cwynn@gascard.net>
2015-02-19 14:49:52 -0600

    Update users.rb

--- mongodb-mysite/recipes/users.rb
+++ mongodb-mysite/recipes/users.rb
@@ -1,5 +1,6 @@
 node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instance][:private_ip]}"
 
+include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::users"
 
 system 'useradd cwynn2 -p'
