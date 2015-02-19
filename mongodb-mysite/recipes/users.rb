7594ecf82ebb5f7560cdf844132d0e07ed6ddbec
cwynn <cwynn@gascard.net>
2015-02-19 15:28:32 -0600

    Update users.rb

--- mongodb-mysite/recipes/users.rb
+++ mongodb-mysite/recipes/users.rb
@@ -1,4 +1,4 @@
-node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instance][:private_ip]}"
+#node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instance][:private_ip]}"
 
 #include_recipe "mongodb::10gen_repo"
 include_recipe "users"
