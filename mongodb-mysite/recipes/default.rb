2e6a63cc6fe1bf06eb3188474bdb689550e593b0
cwynn <cwynn@gascard.net>
2015-02-23 22:17:37 -0600

    Update default.rb

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -1,8 +1,8 @@
-node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instance][:private_ip]}"
+#node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instance][:private_ip]}"
 
 
-include_recipe "mongodb::10gen_repo"
-include_recipe "mongodb::default"
+#include_recipe "mongodb::10gen_repo"
+#include_recipe "mongodb::default"
 
 package 'fail2ban'
 
