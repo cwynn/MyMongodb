95adf8ab4059b7f1d86d1f671a08287d0f4d026a
cwynn <cwynn@gascard.net>
2015-02-24 16:52:15 -0600

    Update users.rb

--- mongodb-mysite/recipes/users.rb
+++ mongodb-mysite/recipes/users.rb
@@ -1,8 +1,3 @@
-#node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instance][:private_ip]}"
-
-#include_recipe "mongodb::10gen_repo"
-include_recipe "mongodb::users"
-
 user_account 'hsolo' do
   comment   'Han Solo'
   home      '/home/hsolo'
