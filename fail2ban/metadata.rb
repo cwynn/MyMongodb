5092eb3f953ac2c05be5faecfd77125f49424eb8
cwynn <cwynn@gascard.net>
2015-02-27 09:11:02 -0600

    Update metadata.rb

--- fail2ban/metadata.rb
+++ fail2ban/metadata.rb
@@ -4,4 +4,10 @@ maintainer  "MyOrg"
 license     "Apache 2.0"
 version     "1.0.0"
 
-depends 'fail2ban'
+#depends 'fail2ban'
+
+recipe 'fail2ban', 'Installs and configures fail2ban'
+
+%w{ debian ubuntu redhat centos scientific amazon oracle fedora}.each do |os|
+  supports os
+end
